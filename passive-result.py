625f5f83aaca4706911e90a2e3e5bdda4e69efa9
Ruben <ruben.dominguez.93@gmail.com>
2017-05-03 12:32:25 +0200

    Corrected uri

--- passive-result.py
+++ passive-result.py
@@ -7,7 +7,7 @@ cmd = "<nagios-plugin-with-args>"
 process = Popen(shlex.split(cmd,posix=False), stdout=PIPE)
 output = process.communicate()[0]
 exit_code = process.wait()
-url = 'https://<icinga2-ip>:5665/v1/actions/process-check-result'
+url = 'https://<icinga2-ip>:5665/v1/actions/process-check-result?service=<host>!<service-name>'
 headers = {'Accept':'application/json'}
 payload = {'filter':'service.name=="<service-name>"','type':'Service','exit_status':exit_code,'plugin_output':output}
 r = requests.post(url,headers=headers,json=payload,verify=False,auth=('<user>','<pass>'))
